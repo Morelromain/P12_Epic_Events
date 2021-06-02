d2c6900573b1e094715a130d19ae330079053248
Romain Morel <morelromain86@gmail.com>
2021-06-02 13:49:56 +0200

    change user permission

--- epic_events/user/perm.py
+++ epic_events/user/perm.py
@@ -18,7 +18,7 @@ class UserPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user == obj:
             return True
-        return False
+        return request.method in permissions.SAFE_METHODS
 
 class GroupPermission(permissions.BasePermission):
     """
