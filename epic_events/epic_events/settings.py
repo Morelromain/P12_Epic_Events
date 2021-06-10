674bccde6300e5fb4f24cfe2991e305a5080b16b
Romain Morel <morelromain86@gmail.com>
2021-06-10 20:43:06 +0200

    modificate datetime format

--- epic_events/epic_events/settings.py
+++ epic_events/epic_events/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATE_FORMAT': '%d-%m-%Y',
-    'DATETIME_FORMAT': '%d-%m-%Y-- %H:%M',
+    'DATETIME_FORMAT': '%d-%m-%Y %H:%M',
     'DEFAULT_AUTHENTICATION_CLASSES': (
     'rest_framework_simplejwt.authentication.JWTAuthentication',
     'rest_framework.authentication.SessionAuthentication',
