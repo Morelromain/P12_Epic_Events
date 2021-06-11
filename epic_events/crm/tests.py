9ae0a69aaae7a4fb57484c6231ab0a405ddc87c9
Romain Morel <morelromain86@gmail.com>
2021-06-11 11:27:44 +0200

    add viewtest

--- epic_events/crm/tests.py
+++ epic_events/crm/tests.py
@@ -1,22 +1,47 @@
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from user.models import User
+from django.contrib.auth.models import Group
 
-class CRMTestCase(APITestCase):
 
-    def test_index(self):
+class CRMViewsTests(APITestCase):
+
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test_mdp'
+        self.data = {
+            'username': self.username,
+            'password': self.password,
+        }
+        self.url_get_token = reverse('token_obtain_pair')
+
+    def test_basic_index_acces(self):
         result = self.client.get("/")
         self.assertEqual(result.status_code, status.HTTP_200_OK)
 
-    def test_acces_login(self):
-        result = self.client.get("/api-auth/login/")
-        self.assertEqual(result.status_code, status.HTTP_200_OK)
+    def test_bad_login_fail_recovers_token(self):
+        user = User.objects.create_user(username='bad_user_name', password='test_mdp')
+        response = self.client.post(self.url_get_token, self.data, format='json')
+        assert response.status_code in [401]
 
-    def test_log(self):
-        data = {"username": "user10", "password": "86Ibra86aaa"}
-        result = self.client.post("/api-auth/login/", data)
-        assert result.status_code in [200]
+    def test_login_with_bad_permission(self):
+        user = User.objects.create_user(username='test_user', password='test_mdp')
+        response = self.client.post(self.url_get_token, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+        token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer {0}'.format(token))
+        response = self.client.get("/clients/", data={'format': 'json'})
+        assert response.status_code in [403]
 
-    def test_acces_logout(self):
-        result = self.client.get("/api-auth/logout/")
-        assert result.status_code in [200]
+    def test_login_and_acces_with_token_GOOD(self):
+        group = Group.objects.create(name='Sales')
+        user = User.objects.create_user(username='test_user', password='test_mdp')
+        user.groups.add(group)
+        response = self.client.post(self.url_get_token, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+        token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer {0}'.format(token))
+        response = self.client.get("/clients/", data={'format': 'json'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
--- epic_events/user/tests.py
+++ epic_events/user/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from user.models import User
+from django.contrib.auth.models import Group
+
+
+class UserViewsTests(APITestCase):
+
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test_mdp'
+        self.data = {
+            'username': self.username,
+            'password': self.password,
+        }
+        self.url_get_token = reverse('token_obtain_pair')
+
+    def test_basic_index_acces(self):
+        result = self.client.get("/")
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_bad_login_fail_recovers_token(self):
+        user = User.objects.create_user(username='bad_user_name', password='test_mdp')
+        response = self.client.post(self.url_get_token, self.data, format='json')
+        assert response.status_code in [401]
+
+    def test_login_with_bad_permission(self):
+        user = User.objects.create_user(username='test_user', password='test_mdp')
+        response = self.client.post(self.url_get_token, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+        token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer {0}'.format(token))
+        response = self.client.get("/clients/", data={'format': 'json'})
+        assert response.status_code in [403]
+
+    def test_login_and_acces_with_token_GOOD(self):
+        group = Group.objects.create(name='Sales')
+        user = User.objects.create_user(username='test_user', password='test_mdp')
+        user.groups.add(group)
+        response = self.client.post(self.url_get_token, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+        token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer {0}'.format(token))
+        response = self.client.get("/users/", data={'format': 'json'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
